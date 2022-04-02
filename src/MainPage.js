ac0427eac4c8c8ef942106f130931aa5968940b4
osampilona <brzicamartina@gmail.com>
2022-04-02 17:31:00 +0200

    Base background is done

---
+++ src/MainPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./MainPage.css";
+import "./style.css";
+
+const MainPage = () => {
+  return (
+    <div className="mainpage__container">
+      <div className="mainpage__container-outer-frame">
+        <div className="mainpage__container-inner-frame"></div>
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
